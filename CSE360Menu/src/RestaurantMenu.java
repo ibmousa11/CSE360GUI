3f9fdf1f2f706e45c005f8f00b105cc131e9e917
ibmou <ibmou@DESKTOP-JPAKL7I>
2022-03-31 18:09:05 -0700

    Added functionality to Place Order button

--- CSE360Menu/src/RestaurantMenu.java
+++ CSE360Menu/src/RestaurantMenu.java
@@ -28,6 +28,8 @@ public class RestaurantMenu {
 	private JPanel cartPanel;
 	private JLabel subtotal;
 	
+	private int placeInLine = 0;
+	
 
 	/**
 	 * Launch the application.
@@ -145,6 +147,46 @@ public class RestaurantMenu {
 		viewCartButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				homePanel.setVisible(false);
+				placeInLine++;
+				
+				JPanel checkoutPanel = new JPanel();
+				checkoutPanel.setLayout(new GridBagLayout());
+				GridBagConstraints checkoutConstraints = new GridBagConstraints();
+				
+				checkoutPanel.setSize(1920,1080);
+		        
+		        //creating the title, set the constraints
+		        //constraints.fill = GridBagConstraints.HORIZONTAL;
+				checkoutConstraints.gridx = 0;
+				checkoutConstraints.gridy = 0;
+		        checkoutConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+		        checkoutConstraints.weighty = 2;
+		        
+				JLabel OrderPlacedTitle = new JLabel("Order Placed");
+				//OrderPlacedTitle.setSize(290, 70);
+				OrderPlacedTitle.setFont(new Font("Impact", Font.PLAIN, 80));
+				checkoutPanel.add(OrderPlacedTitle,checkoutConstraints);
+				
+				JLabel placeInLineLabel = new JLabel("Your place in line is: " + placeInLine);
+				//placeInLineLabel.setSize(290, 70);
+				placeInLineLabel.setFont(new Font("Impact", Font.PLAIN, 20));
+				checkoutConstraints.gridy = 1;
+				checkoutPanel.add(placeInLineLabel,checkoutConstraints);
+				
+				
+				JPanel orderSummaryPanel = new JPanel();
+				checkoutConstraints.ipadx = 200;
+				checkoutConstraints.ipady = 300;
+				orderSummaryPanel.setBorder(BorderFactory.createTitledBorder("Summary"));
+				checkoutConstraints.gridy = 2; 
+				checkoutConstraints.fill = GridBagConstraints.HORIZONTAL;
+			
+				checkoutPanel.add(orderSummaryPanel,checkoutConstraints);
+				
+				frame.add(checkoutPanel);
+				
+				
 			}
 		});
 		
@@ -157,7 +199,7 @@ public class RestaurantMenu {
         subtotal = new JLabel("Total: $" + String.valueOf(currentUser.getOrdersArr()[currentUser.getNumOfOrders()].getTotalPrice()));
         homePanel.add(subtotal,constraints);
 		/* ----------------------------------------------------------------------------------------- */
-
+	
 
 		
 		/* -------------------------------------- Coupon Panel ------------------------------------- */
